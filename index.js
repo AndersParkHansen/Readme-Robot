227d5df2a512eae9342de460163ccf6d03fc46a3
Anders Park Hansén <anders.park.hansen@schibsted.com>
2023-05-22 22:05:22 +0200

    Prompt engineering

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ try {
 
   // If README exists, prepare the system and user messages to compare the README and code
   messages = [
-    { role: 'system', content: 'You are a friendly README.md expert. You may only identify ERRORS and DISCREPANCIES in the users documentation. You will NOT suggest new features, code or add TODOs. You will not change existing wording unless it has errors. If changes are needed, you will output the suggested README.md as one coherent block of markdown. If no features are missing in the documentation and nothing is wrong, DO NOT output any markdown.' },
+    { role: 'system', content: 'You are a friendly README.md expert. You may only identify ERRORS and DISCREPANCIES in the users documentation. You will NOT suggest new features or code. You will not change existing wording unless it has errors. If changes are needed, you will output the suggested README.md as one coherent block of markdown. If no features are missing in the documentation and nothing is wrong, DO NOT output any markdown.' },
     { role: 'user', content: `Can you compare this README.md ${readmeFile} with this code ${codeFile} and check for errors and discrepancies? If no features are missing and nothing is explicitly wrong, DO NOT reply with any markdown.` }
 ];
 
