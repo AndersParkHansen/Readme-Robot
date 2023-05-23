0e488bb2be8d21fe3071b9476678131314039e08
Anders Park Hansén <anders.park.hansen@schibsted.com>
2023-05-23 10:29:15 +0200

    Started implementing pull req logic

--- index.js
+++ index.js
@@ -6,6 +6,16 @@ require('dotenv').config();
 const fs = require('fs');
 const { Configuration, OpenAIApi } = require('openai');
 
+const { Octokit } = require("@octokit/rest");
+const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN });
+
+const owner = 'owner'; // Replace with your GitHub username
+const repo = 'repo'; // Replace with your repository name
+const base = 'main'; // The base branch for the pull request
+const head = 'bot-requests'; // The branch to create
+const path = 'README.md'; // The file to update
+
+
 // Configure the OpenAI API client with API key
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
@@ -37,6 +47,20 @@ try {
   ];
 }
 
+function extractMarkdown(apiResponse) {
+  const content = apiResponse.data.choices[0].message.content;
+  const markdownStart = content.indexOf("```markdown");
+  const markdownEnd = content.lastIndexOf("```");
+
+  if (markdownStart === -1 || markdownEnd === -1) {
+    console.log("No markdown found in response.");
+    return;
+  }
+
+  return content.substring(markdownStart + "```markdown".length, markdownEnd);
+}
+
+
 async function createChatCompletion() {
   // Call the OpenAI API with the prepared messages
   const response = await openai.createChatCompletion({
@@ -45,10 +69,28 @@ async function createChatCompletion() {
     max_tokens: 2048,
     temperature: 0.0,
   });
-
   // Log the suggested changes or new README content
   console.log(`Suggested changes or generated README content: ${response.data.choices[0].message.content}`);
 }
 
 // Call the function to start the process
 createChatCompletion();
+
+
+// Step 1: Get the SHA of the latest commit on the base branch
+const { data: { object: { sha: baseSha }}} = await octokit.git.getRef({ owner, repo, ref: `heads/${base}` });
+
+// Step 2: Create a new branch
+await octokit.git.createRef({ owner, repo, ref: `refs/heads/${head}`, sha: baseSha });
+
+// Step 3: Get the content of the existing README.md file
+const { data: { content: oldContent, sha: oldSha }} = await octokit.repos.getContent({ owner, repo, path });
+
+// Prepare the new content. You should replace this with your new README content.
+const newContent = Buffer.from(oldContent + '\nNew content').toString('base64');
+
+// Step 4: Update the README.md file on the new branch
+await octokit.repos.createOrUpdateFileContents({ owner, repo, path, message: 'Update README.md', content: newContent, sha: oldSha, branch: head });
+
+// Step 5: Create a pull request
+await octokit.pulls.create({ owner, repo, title: 'Update README.md', head, base });
