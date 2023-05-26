eeb840bd43181ae40237bd8e60d423bb06598a02
Anders Park Hansén <anders.park.hansen@schibsted.com>
2023-05-26 08:04:01 +0200

    Added API mocking for tests

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+let useAPI = true; // set this to false to use mock responses
+
 // Load environment variables with API keys
 require('dotenv').config();
 
@@ -12,7 +14,7 @@ const owner = 'AndersParkHansen'; // Replace with your GitHub username
 const repo = 'openai-check-the-docs'; // Replace with your repository name
 const base = 'main'; // The base branch for the pull request
 const head = 'bot-requests'; // The branch to create
-const path = 'README.md'; // The file to update
+const path = require('path');
 
 
 // Configure the OpenAI API client with API key
@@ -27,6 +29,7 @@ const codeFile = fs.readFileSync('index.js', 'utf-8');
 
 let readmeFile;
 let messages;
+
 
 try {
   // Try to read an existing README.md file
@@ -47,58 +50,99 @@ try {
 }
 
 function extractMarkdown(apiResponse) {
-  const content = apiResponse.data.choices[0].message.content;
+  const content = apiResponse.data.choices[0].message.content.trim();
 
-  // Regular expression to match markdown blocks
-  const markdownBlockRegex = /```markdown\n([\s\S]*?)\n```(?![\s\S]*```)/s;
-
-  // Match all markdown blocks
-  const markdownBlocks = content.match(markdownBlockRegex);
-
-  if (markdownBlocks === null) {
-    console.log("No specific markdown block found in response. Returning the full content.");
+  // 1. If the response starts with '#', assume it's a markdown-only response and return as is
+  if (content.startsWith('#')) {
+    console.log("The response only contains markdown and starts with '#'. Returning the content as is.");
     return content;
   }
 
-  // If markdownBlocks is not null, it's an array and we can map over it
-  if (markdownBlocks) {
-    // Return all markdown blocks, excluding the leading "```markdown" and trailing ```
-    // The group in parentheses in the regex captures the part we want
-    const markdowns = markdownBlocks.map(block => block.slice(12, -3)); // slice to remove leading and trailing markdown syntax
-    
-    // Join all the markdown blocks into a single string before returning
-    return markdowns.join('\n');
+  // 2. If the response contains triple backticks, assume it's a markdown response with comments
+  const firstBacktickIndex = content.indexOf('```');
+  const lastBacktickIndex = content.lastIndexOf('```');
+
+  if (firstBacktickIndex !== -1 && lastBacktickIndex !== -1) {
+    console.log("Triple backticks found in the response. Extracting the markdown content.");
+
+    // Extract the content between the first and last triple backtick
+    const markdownContent = content.slice(firstBacktickIndex, lastBacktickIndex + 3); // +3 to include the last triple backticks
+
+    // Remove the starting and ending triple backticks and 'markdown' after the first set of backticks
+    const strippedMarkdownContent = markdownContent
+      .replace(/^```markdown\n/, '') // remove starting triple backtick and 'markdown'
+      .replace(/```$/, ''); // remove ending triple backtick
+
+    return strippedMarkdownContent;
   }
+
+  // 3. If the response doesn't fit the previous cases, assume it only contains text and no markdown. Log the response and return null
+  console.log("The response only contains text and no markdown. No pull request will be created.");
+  console.log("API Response:", content);
+  
+  return null;
 }
+
+
+
+
 
 
 
 
 async function createChatCompletion() {
-  // Call the OpenAI API with the prepared messages
-  const response = await openai.createChatCompletion({
-    model: 'gpt-3.5-turbo',
-    messages: messages,
-    max_tokens: 2048,
-    temperature: 0.0,
-  });
+  let response;
+  let content;
+
+  if (useAPI) {
+    // Call the OpenAI API with the prepared messages
+    response = await openai.createChatCompletion({
+      model: 'gpt-4',
+      messages: messages,
+      max_tokens: 2048,
+      temperature: 0.0,
+    });
   
+    if (!response.data || !response.data.choices || !response.data.choices[0] || !response.data.choices[0].message) {
+      console.error("Unexpected API response format.");
+      console.error(response);
+      return;
+    }
+      
+  } else {
+    // Use the appropriate mock response
+    // response = getMockResponse('markdownWithComments.txt'); // adjust this based on the test case
+    // response = getMockResponse('onlyMarkdown.txt'); // adjust this based on the test case
+    response = getMockResponse('onlyText.txt'); // adjust this based on the test case
+  }
+  
+
   // Extract markdown from API response
-   const markdown = extractMarkdown(response);
-  //const markdown = response.data.choices[0].message.content;
-  
-// If markdown is returned, create pull request
-if (markdown) {
-  console.log("Before replacement:", markdown);
-  //const formattedMarkdown = markdown.replace(/```/g, '~~~');
-  //console.log("After replacement:", formattedMarkdown);
-  // await createPullRequest(markdown);
+  const markdown = extractMarkdown(response);
+
+  // If markdown is returned, create pull request
+  if (markdown) {
+    console.log(markdown);
+    // await createPullRequest(markdown);
+  }
 }
 
-  // Log the suggested changes or new README content
-  console.log(`Suggested changes or generated README content: ${response.data.choices[0].message.content}`);
+function getMockResponse(filename) {
+  const filePath = path.join(__dirname, 'mockApiResponses', filename); 
+  const content = fs.readFileSync(filePath, 'utf8');
+  // Format the content to match the structure of a real API response
+  return {
+    data: {
+      choices: [
+        {
+          message: {
+            content: content
+          }
+        }
+      ]
+    }
+  };
 }
-
 
 // Call the function to start the process
 createChatCompletion();
